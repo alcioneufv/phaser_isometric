acf293456af2b298006f66fed84b94eb66afc856
alcioneufv <95593589+alcioneufv@users.noreply.github.com>
2022-02-23 16:55:35 -0300

    Update iso.js

--- iso.js
+++ iso.js
@@ -152,7 +152,7 @@ class Example extends Phaser.Scene
         this.buildMap();
         this.placeHouses();
 
-        agente = this.add.existing(new Skeleton(this, 300, 0, 'walk', 'south', 380));
+        agente = this.add.existing(new Skeleton(this, 600, 0, 'walk', 'south', 380));
 
         this.cameras.main.setSize(1600, 600);
     }
